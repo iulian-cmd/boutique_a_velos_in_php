70d375f40319c72ed5a33c603fd3f3de86a552a9
iulian <75692549+iulian-cmd@users.noreply.github.com>
2021-08-25 16:43:46 +0200

    Update clients.php

--- clients.php
+++ clients.php
@@ -5,7 +5,7 @@ require ('classe.php');
 $client = new ListeClients;
 
 // On se connecte à MySQL
-$bdd = new PDO('mysql:host=localhost:3307;dbname=boutique_a_velos;charset=utf8', 'myself001', 'guraluimamata');
+$bdd = new PDO('mysql:host=localhost:3307;dbname=boutique_a_velos;charset=utf8', '*******', '**********');
     $reponse = $bdd->query('SELECT * FROM clients'  );
     while ($donnees = $reponse->fetch()){
         $client->ListeClients[]=new Client($donnees['idClient'],$donnees['nomClient'],$donnees['adresseClient'],$donnees['codePostal'],$donnees['ville']);
